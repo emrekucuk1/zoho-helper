f91b2bbc67e27e3ca7bbe269003790422a4bba04
emrekucuk1 <emrekck.34@gmail.com>
2021-12-31 11:26:41 +0300

    adding index zoho books

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const People = require("./people/index");
 const Orchestly = require("./orchestly/index");
 const QNTRL = require("./qntrl/index");
+const Books = require("./books/index");
 const Zoho = require("./zoho/auth.zoho");
 
 module.exports = {
     People,
     QNTRL,
     Orchestly,
+    Books,
     Zoho
 };
