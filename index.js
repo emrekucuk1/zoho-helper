cbebf8a9e486cfec5965527975b8fed5df6738d8
e.kucuk <emre.kucuk@sigmasoftwares.com>
2022-02-14 14:07:53 +0300

    adding version

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const People = require("./people/index");
 const Orchestly = require("./orchestly/index");
 const QNTRL = require("./qntrl/index");
 const Books = require("./books/index");
+const Expense = require("./expense/index");
 const Zoho = require("./zoho/auth.zoho");
 
 module.exports = {
@@ -9,5 +10,6 @@ module.exports = {
     QNTRL,
     Orchestly,
     Books,
+    Expense,
     Zoho
 };
