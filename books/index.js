e117ec9dbeed351fb2bc4f3cadade0a2daee4388
e.kucuk <emre.kucuk@sigmasoftwares.com>
2022-02-21 09:56:24 +0300

    adding books get currencies

--- books/index.js
+++ books/index.js
@@ -112,6 +112,17 @@ class Books extends ZohoAuth {
         }
     }
 
+    async getCurrencies(org_id) {
+        try {
+            return await this.customRequestV2(`https://books.zoho.com/api/v3/settings/currencies?organization_id=${org_id}`, "GET");
+        }catch (e) {
+            if (e.response !== undefined)
+                console.error(e.response.data);
+            else
+                console.error(e.message);
+        }
+    }
+
 }
 
 
