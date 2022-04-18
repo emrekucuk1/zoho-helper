d2a4c33121b3d5920389b069d9e5def97d55d328
e.kucuk <emre.kucuk@sigmasoftwares.com>
2022-04-18 12:24:08 +0300

    add zoho books delete balance

--- books/index.js
+++ books/index.js
@@ -316,6 +316,24 @@ class Books extends ZohoAuth {
         }
     }
 
+    /**
+     *
+     * @param org_id
+     * @param parameters
+     * @returns {Promise<*|undefined>}
+     */
+    async deleteBalance(org_id,parameters) {
+        const token = await this.getToken();
+        try {
+            return await this.customRequestV6(`https://books.zoho.com/api/v3/settings/openingbalances?organization_id=${org_id}`, 'DELETE',parameters)
+        }catch (e) {
+            if (e.response !== undefined)
+                console.log(e.response.data);
+            else
+                console.error(e.message)
+        }
+    }
+
 
 }
 
